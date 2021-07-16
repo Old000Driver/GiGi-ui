4c2b2363603fac95d8d87c7a7f65df0b48118885
Gja <9206150+gja_001@user.noreply.gitee.com>
2021-07-16 16:15:19 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,5 +2,15 @@
   <div>
     button实例
   </div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
 
+export default {
+  components:{Button:Button}
+}
+</script>
