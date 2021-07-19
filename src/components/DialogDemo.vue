f21cc103e1c5760235e246c9997eaec33a1d9f3f
Gja <9206150+gja_001@user.noreply.gitee.com>
2021-07-19 16:09:10 +0800

    dialog 支持自定义title和content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -5,7 +5,15 @@
   <!--    <Dialog :visible="x" @update:visible="x = $event"></Dialog>-->
   <Dialog v-model:visible="x" :closeOnClickOverlay="false"
           :ok="f1" :cancel="f2"
-  ></Dialog>
+  >
+    <template v-slot:content>
+      <strong>hi</strong>
+      <div>hi2</div>
+    </template>
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
 </template>
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,12 @@
          @click="onClickOverlay"></div>
     <div class="gulu-dialog-wrapper">
       <div class="gulu-dialog">
-        <header>标题
+        <header>
+          <slot name="title"/>
           <span @click="close" class="gulu-dialog-close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content" />
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
